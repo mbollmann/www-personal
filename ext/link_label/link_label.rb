b88414272e0808b51366db0be6545551eca59be9
Marcel Bollmann <bollmann@linguistics.rub.de>
2015-10-05 15:48:55 +0200

    Add LinkLabel extension, add icons to PDF links

---
+++ ext/link_label/link_label.rb
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+require 'webgen/tag'
+
+module Webgen
+  class Tag
+    module LinkLabel
+      def self.call(tag, body, context)
+        result = "["
+        # insert icon
+        result << "<i class=\"fa fa-" << context[:config]['tag.link_label.icon']
+        result << "\"></i> "
+        # insert text
+        result << context[:config]['tag.link_label.text']
+        # insert link
+        result << "](" << context[:config]['tag.link_label.path']
+        result << "){:.label.label-info}"
+        result
+      end
+    end
+  end
+end
